d2de88ef2af8f4bda411df343be50274e6c9dc90
deepak-ysoft <deepaksysoft@gmail.com>
2024-09-04 17:25:17 +0530

    aasa

---
+++ Product/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Product.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Estimated_Complexity Complexity { get; set; }
+        public Status status { get; set; }
+        public DateTime TargateComplactionDate { get; set; }
+        public DateTime ActualComplactionDate { get; set; }
+    }
+    public enum Status
+    {
+        New = 0,
+        Active = 1,
+        Closed = 2,
+        Abandoned = 3,
+    }
+}
