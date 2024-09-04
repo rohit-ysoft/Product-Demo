fa989d85aa88963f8845d215010d29aa3955cba5
rohit-ysoft <rohits@ysoftsolution.com>
2024-09-04 17:37:27 +0530

    sdsdwd

---
+++ Product/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()  
+        {
+            return View();
+        }
+    }
+}
