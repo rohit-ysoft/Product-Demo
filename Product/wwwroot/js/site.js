617eab4f1c465b302b2f57f334db2937c01bc559
rohit-ysoft <rohits@ysoftsolution.com>
2024-09-05 12:18:37 +0530

    erfefef

--- Product/wwwroot/js/site.js
+++ Product/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+// Add Data Function
+
+$(".button").click(function () {
+    var id = $(this).attr('id');
+    $.ajax({
+        url: "@Url.Action("Create", "Product")",
+        type: "GET",
+        dataType: "html",
+        data: { id: id },
+        success: function (data) {
+            $("#modal_" + id).html(data);//target position of modal
+            $('#pqr_' + id).modal('toggle');
+        }
+    });
+});
+
+public ActionResult GetDataForPopUpWindow(int id)
+{
+    using(YourDBContext)
+    {
+        var data = YourData;
+      pqr pqrModel = new pqr();
+        pqrModel.YourModelProperty = data.CorrespondingProperty;
+        return PartialView("_GridPartialView", pqrModel);
+    }
+}
